c2882f69c400c76d330af5d11ced925594329b63
tangyong <tangyong@cn.fujitsu.com>
2013-10-09 17:25:11 +0900

    Committing CDI Basic Sample 01

---
+++ UnderstandingCDI/basic/src/main/java/tangyong/javaee/understadingcdi/basic01/FiveStarHotel.java
@@ -0,0 +1,10 @@
+package tangyong.javaee.understadingcdi.basic01;
+
+public class FiveStarHotel implements Hotel{
+
+	@Override
+	public String getName() {
+		return "FiveStarHotel";
+	}
+
+}
---
+++ UnderstandingCDI/basic/src/main/java/tangyong/javaee/understadingcdi/basic01/Hotel.java
@@ -0,0 +1,6 @@
+package tangyong.javaee.understadingcdi.basic01;
+
+public interface Hotel {
+	
+	public String getName();
+}
